db5e09bdc5718bc4850d0bc7d0bf4d82b5cf6292
KediBedri <hudkuzeyli@gmail.com>
2024-12-19 20:11:29 +0300

    Add project files.

---
+++ Models/BaseObject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace intro_LoginExam_v1.Models
+{
+	public class BaseObject
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[DisplayName("Sıra No")]
+		public int Id { get; set; }
+
+		[DisplayName("Durum")]
+		[Required(ErrorMessage = "Seçiniz..")]
+		public bool Aktif { get; set; }
+
+		[ScaffoldColumn(false)]//Tablo üzerinde referans gösterilen bir alanın üst modele bağlanmayacağını söylüyoruz.(Not: Biz istediğimiz zaman model üzerinden bu veriyi alabiliyor olacağız)
+		[Browsable(false)]//bu model, bir grid ya da table a datasource oalrak bağlanmışsa arayüzde göster/gösterme demek
+		public string AktifString => Aktif ? "Aktif" : "Pasif";
+	}
+}
---
+++ Models/Kullanici.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace intro_LoginExam_v1.Models
+{
+	public class Kullanici : BaseObject
+	{
+		[Required(ErrorMessage = "{0} boş geçilemez.")]
+		[Display(Name = "Ad Soyad")]//grid de görünecek ya da edit sayfasında görünecek label üzerinde yazan yazı
+		[StringLength(50, ErrorMessage = "{0} alanı en fazla {1} karakter olabilir")]
+		public string AdSoyad { get; set; }
+
+		[Required(ErrorMessage = "{0} boş geçilemez.")]
+		[Display(Name = "E-Mail")]
+		[StringLength(50, ErrorMessage = "{0} adresi en fazla {1} karakterden oluşabilir.")]
+		//	[DataType(DataType.EmailAddress)]
+		[EmailAddress]
+		public string EMail { get; set; }
+
+		[Display(Name = "Kullanıcı Kodu")]
+		[Required(ErrorMessage = "{0} alanı boş olamaz.")]
+		[StringLength(10, ErrorMessage = "{0} en fazla {1} karakterden oluşabilir.")]
+
+		public string KullaniciKodu { get; set; }
+
+		[Display(Name = "Şifre")]
+		[Required(ErrorMessage = "{0} alanı boş olamaz.")]
+		[DataType(DataType.Password)]
+		//en az 2 rakam, maks 10 elamn, min 1 tane özel , min 1 büyük harf
+		[RegularExpression(@"^(?=(.*\d){2,})(?=.*[\W_])(?=.*[A-Z]).{1,10}$")]
+		[StringLength(10, ErrorMessage ="{0} alanı {1} karakterden az olamaz.")]
+		public string KullaniciSifre { get; set; }
+
+		[RegularExpression("^[0-9]+$", ErrorMessage ="{0} alanına sadece sayısal değer yazabilirsiniz.")]
+		public string Yas { get; set; }//565464654
+
+
+	}
+}
