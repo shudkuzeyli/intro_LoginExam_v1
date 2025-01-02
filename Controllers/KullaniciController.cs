0d4f8d5655aafefe55ed21090f8e593ff7031215
KediBedri <hudkuzeyli@gmail.com>
2025-01-02 20:08:11 +0300

    liste sayfası oluşturuluyor

---
+++ Controllers/KullaniciController.cs
@@ -0,0 +1,24 @@
+﻿using intro_LoginExam_v1.Context;
+using intro_LoginExam_v1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace intro_LoginExam_v1.Controllers
+{
+	public class KullaniciController : Controller
+	{
+
+		private readonly DataContext _dataContext;
+
+		public KullaniciController(Context.DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+
+		public IActionResult Index()
+		{
+			var kullanici = _dataContext.Kullanici.ToList();
+
+			return View(kullanici);
+		}
+	}
+}
